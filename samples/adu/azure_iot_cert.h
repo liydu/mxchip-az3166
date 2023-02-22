d0394366429f37d1b1c62bb2af2c5ae057588d59
Liki Du <liki@likidu.com>
2023-02-21 22:56:22 -0800

    WIP ADU support

---
+++ samples/adu/azure_iot_cert.h
@@ -0,0 +1,16 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
+#ifndef _AZURE_IOT_CERT_H
+#define _AZURE_IOT_CERT_H
+
+extern const unsigned char azure_iot_root_cert[];
+extern const unsigned int azure_iot_root_cert_size;
+
+extern const unsigned char azure_iot_root_cert_2[];
+extern const unsigned int azure_iot_root_cert_size_2;
+
+extern const unsigned char azure_iot_root_cert_3[];
+extern const unsigned int azure_iot_root_cert_size_3;
+
+#endif
